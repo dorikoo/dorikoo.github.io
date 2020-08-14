b6e6aae5aa5c7ed1888fd342db9e6dbe7fdf397d
dorikoo <dorikoo@gmail.com>
2020-08-14 17:04:57 +0900

    portfolio update animation01

--- js/common.js
+++ js/common.js
@@ -1,4 +1,35 @@
 $(document).ready(function(){
+	//메인 타이핑
+	var typingBool = false; 
+    var typingIdx=0; 
+    var typingTxt = $(".typing-txt").text(); // 타이핑될 텍스트를 가져온다 
+    typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
+    if(typingBool==false){ // 타이핑이 진행되지 않았다면 
+       typingBool=true; 
+       
+       var tyInt = setInterval(typing,70); // 반복동작 
+     } 
+     
+     function typing(){ 
+       if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
+		   
+		  if(typingTxt[typingIdx]=="아"){
+			 $(".typing").append("<br>"); 
+			  typingIdx++; 
+		  }
+		  else if(typingTxt[typingIdx]=="어"){
+			 $(".typing").append("<span class=\"point\">김도연</span>");
+			  typingIdx++; 
+		  }
+		  else{
+         	$(".typing").append(typingTxt[typingIdx]);
+         	typingIdx++; 
+		  }
+       } else{ 
+         clearInterval(tyInt); //끝나면 반복종료 
+       } 
+     } 
+	
 	$('.menu_bar a').click(function(){
 		$('.menu_bar').toggleClass('active');
 	});
@@ -20,29 +51,9 @@ $(document).ready(function(){
             $('footer').removeClass('active');
         }
     });
-	
-	$(".contact_link ul li a").hover(function(){
-		var value1 = $(this).find('img').attr('class');
-		
-		if(value1=="email"){
-			$(this).html('<img src="img/common/f_email_h.png" class="email_h" alt="">');
-		}
-		else if(value1=="profile"){
-			$(this).html('<img src="img/common/f_profile_h.png" class="profile_h" alt="">');
-		}
-	},function(){
-		var value2 = $(this).find('img').attr('class');
-		
-		if(value2=="email_h"){
-			$(this).html('<img src="img/common/f_email.png" class="email" alt="">');
-		}
-		else if(value2=="profile_h"){
-			$(this).html('<img src="img/common/f_profile.png" class="profile" alt="">');
-		}
-	});
     
-    $('.return').click(function(){
-        alert("준비중 입니다.");
+    $('.return, .profile_link').click(function(){
+        alert("준비 중 입니다.");
         return false;
     });
     
