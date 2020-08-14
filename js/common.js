f6a5dd8b239c6d95fd8ed17220b818cbb191737d
dorikoo <dorikoo@gmail.com>
2020-08-14 17:32:31 +0900

    portfolio update animation

--- js/common.js
+++ js/common.js
@@ -1,34 +1,48 @@
 $(document).ready(function(){
-	//메인 타이핑
 	var typingBool = false; 
-    var typingIdx=0; 
-    var typingTxt = $(".typing-txt").text(); // 타이핑될 텍스트를 가져온다 
-    typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
-    if(typingBool==false){ // 타이핑이 진행되지 않았다면 
-       typingBool=true; 
-       
-       var tyInt = setInterval(typing,70); // 반복동작 
-     } 
-     
-     function typing(){ 
-       if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
-		   
-		  if(typingTxt[typingIdx]=="아"){
-			 $(".typing").append("<br>"); 
-			  typingIdx++; 
-		  }
-		  else if(typingTxt[typingIdx]=="어"){
-			 $(".typing").append("<span class=\"point\">김도연</span>");
+	var typingIdx=0; 
+	var liIndex = 0;
+	var liLength = $(".typing-txt>ul>li").length;
+
+
+	var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
+	typingTxt=typingTxt.split("");  
+	if(typingBool==false){ 
+		typingBool=true; 
+		var tyInt = setInterval(typing,100); 
+	} 
+
+	function typing(){ 
+	  $(".typing ul li").removeClass("on");
+	  $(".typing ul li").eq(liIndex).addClass("on");
+	  if(typingIdx<typingTxt.length){
+		  if(typingTxt[typingIdx]=="어"){
+			 $(".typing ul li").eq(liIndex).append("<span class=\"point\">김도연</span>");
 			  typingIdx++; 
 		  }
 		  else{
-         	$(".typing").append(typingTxt[typingIdx]);
+			$(".typing ul li").eq(liIndex).append(typingTxt[typingIdx]);
          	typingIdx++; 
 		  }
-       } else{ 
-         clearInterval(tyInt); //끝나면 반복종료 
-       } 
-     } 
+	   }
+	   else{ 
+		   if(liIndex<liLength-1){
+				liIndex++; 
+				typingIdx=0;
+				typingBool = false; 
+				typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
+
+				clearInterval(tyInt);
+
+				setTimeout(function(){
+				  tyInt = setInterval(typing,100);
+				},1000);
+			}
+		   else if(liIndex==liLength-1){
+			   clearInterval(tyInt);
+		   }
+		} 
+	}    
 	
 	$('.menu_bar a').click(function(){
 		$('.menu_bar').toggleClass('active');
